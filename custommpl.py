b09eaa002b6cf0869e6acac359830a0a19c8292b
Astro-Totzauer <68924385+Astro-Totzauer@users.noreply.github.com>
2020-09-14 14:27:45 -1000

    Add files via upload

--- custommpl.py
+++ custommpl.py
@@ -12,7 +12,8 @@ import savitzkygolay
 import numpy as np
 from pathlib import Path
 from astropy.io import fits
-from astropy.visualization import ContrastBiasStretch,ManualInterval,LinearStretch,MinMaxInterval,ImageNormalize, SqrtStretch,LogStretch,PowerDistStretch,PowerStretch,SinhStretch,SquaredStretch,AsinhStretch,PercentileInterval
+from astropy.visualization import ContrastBiasStretch,ManualInterval,LinearStretch,MinMaxInterval,ImageNormalize, SqrtStretch,LogStretch,PowerDistStretch,PowerStretch,SinhStretch,SquaredStretch,AsinhStretch,PercentileInterval,AsymmetricPercentileInterval,ZScaleInterval, BaseStretch
+from astropy.wcs import WCS
 from scipy.signal import savgol_filter, medfilt
 # Connects the Qt UI file with this python file
 Ui_MainWindow, QMainWindow = loadUiType('window.ui')
@@ -28,6 +29,12 @@ class Main(QMainWindow, Ui_MainWindow):
         self.var_min = 0
         self.var_max = 1
         self.var_int = 1
+        self.v_min = 0
+        self.v_max = 1
+        self.percent = 0
+        self.percent_low = 0
+        self.percent_high = 100
+        self.stretch_val = BaseStretch()
         self.varSlider.setMinimum(self.var_min)
         self.varSlider.setMaximum(self.var_max)
         self.varSlider.setTickInterval(self.var_int)
@@ -35,21 +42,38 @@ class Main(QMainWindow, Ui_MainWindow):
         self.rbtn1.toggled.connect(self.noise_reduction_select)
         self.rbtn2.toggled.connect(self.noise_reduction_select)
         self.rbtn3.toggled.connect(self.noise_reduction_select)
+        self.rbtn5.toggled.connect(self.interval_select)
+        self.rbtn6.toggled.connect(self.interval_select)
+        self.rbtn7.toggled.connect(self.interval_select)
+        self.rbtn8.toggled.connect(self.interval_select)
+        self.rbtn9.toggled.connect(self.interval_select)
+        self.rbtn4.toggled.connect(self.cube_mean)
+        self.meantxt.returnPressed.connect(self.set_mean_window)
         self.sgwintxt.returnPressed.connect(self.set_sg_window)
         self.sgdegtxt.returnPressed.connect(self.set_sg_degree)
         self.rdiffCheck.stateChanged.connect(self.run_diff)
+        self.divCheck.stateChanged.connect(self.cube_div)
+        self.vmintxt.returnPressed.connect(self.set_vmin)
+        self.vmaxtxt.returnPressed.connect(self.set_vmax)
+        self.percenttxt.returnPressed.connect(self.set_percent)
+        self.percentlowtxt.returnPressed.connect(self.set_percent_low)
+        self.percenthightxt.returnPressed.connect(self.set_percent_high)
         #self.setSGWindow.returnPressed.connect(self.set_sg_window)
         #self.setSGDeg.clicked.connect(self.set_sg_degree)
         self.i = 0
         self.a = 1
+        self.ix = 0
+        self.iy = 0
         self.norm = None
+        self.interval = None
         self.dates = []
         self.sg_window = 5
         self.sg_degree = 3
         self.fig1 = Figure()
         self.canvas = FigureCanvas(self.fig1)
         self.mplvl.addWidget(self.canvas)
-        self.axf1 = self.canvas.figure.subplots(ncols=1,nrows=1)
+        #self.axf1 = self.canvas.figure.subplots(ncols=1,nrows=1)
+        self.cid = self.canvas.mpl_connect('button_press_event', self.figure_coords)
 
 # Function initiated by any key press event
     def keyPressEvent(self, event):
@@ -63,6 +87,42 @@ class Main(QMainWindow, Ui_MainWindow):
         if isinstance(focused_widget, QtWidgets.QLineEdit):
             focused_widget.clearFocus()
         
+    def set_percent(self):
+        self.percent = int(self.percenttxt.text())
+        main.percent_int
+        
+    def percent_int(self):
+        self.interval = PercentileInterval(self.percent)
+        self.refresh_norm()
+    
+    def set_vmin(self):
+        self.v_min = self.vmintxt.text()
+        main.manual_int()
+    
+    def set_vmax(self):
+        self.v_max = self.vmaxtxt.text()
+        main.manual_int()
+
+    def manual_int(self):
+        self.interval = ManualInterval(self.v_min, self.v_max)
+        main.refresh_norm()
+
+    def set_percent_low(self):
+        self.percent_low = self.percentlowtxt.text()
+        main.refresh_norm()
+
+    def set_percent_high(self):
+        self.percent_high = self.percenthightxt.text()
+        main.asym_int()
+
+    def asym_int(self):
+        self.interval = AsymmetricPercentileInterval(self.percent_low, self.percent_high)
+        main.refresh_norm()
+        print(self.percent_low, self.percent_high)
+
+    def refresh_norm(self):
+        self.norm = ImageNormalize(interval=self.interval, stretch=self.stretch_val, clip=True)
+        main.create_image(self.cube)
 
 # Contextual slider behavior, for stretch functions
     def setSliderValue(self):
@@ -76,11 +136,19 @@ class Main(QMainWindow, Ui_MainWindow):
             self.i = 0
             self.cube = np.diff(self.cube, axis=2)
             self.n = self.cube.shape[2]
-            self.create_image(self.cube)
+            main.create_image(self.cube)
         else:
             self.cube = self.cube_base
             self.n = self.cube.shape[2]
-            self.create_image(self.cube)
+            main.create_image(self.cube)
+
+    def cube_div(self, state):
+        if state == Qt.Checked:
+            self.cube = self.cube_base / self.cube
+            main.create_image(self.cube)
+        else:
+            self.cube = self.cube_base
+            main.create_image(self.cube)
 
     def set_sg_window(self):
         self.sg_window = int(self.sgwintxt.text())
@@ -91,6 +159,11 @@ class Main(QMainWindow, Ui_MainWindow):
         self.sg_degree = int(self.sgdegtxt.text())
         main.savitzky_golay(self.sg_window, self.sg_degree)
         self.sgdegtxt.clearFocus()
+    
+    def set_mean_window(self):
+        self.mean_window = int(self.meantxt.text())
+        main.cube_mean(self.mean_window)
+        self.meantxt.clearFocus()
 
     def noise_reduction_select(self):
         radioBtn = self.sender()
@@ -102,6 +175,26 @@ class Main(QMainWindow, Ui_MainWindow):
             elif radioBtn.text() == 'None':
                 main.no_filter()
 
+    def interval_select(self):
+        radioBtn = self.sender()
+        if radioBtn.isChecked():
+            if radioBtn.text() == 'MinMax':
+                self.interval = MinMaxInterval()
+                main.refresh_norm()
+            elif radioBtn.text() == 'Manual':
+                main.manual_int()
+                main.refresh_norm()
+            elif radioBtn.text() == 'Percentile':
+                main.percent_int()
+                main.refresh_norm()
+            elif radioBtn.text() == 'AsymmetricPercentile':
+                main.asym_int()
+                main.refresh_norm()
+            elif radioBtn.text() == 'ZScale':
+                self.interval = ZScaleInterval()
+                main.refresh_norm()
+
+     
 
     def savitzky_golay(self, x, y):
         self.cube = savitzkygolay.filter3D(self.cube_base, self.sg_window, self.sg_degree)
@@ -115,6 +208,25 @@ class Main(QMainWindow, Ui_MainWindow):
         self.cube = self.cube_base
         main.create_image(self.cube)
 
+    def cube_mean(self, window):
+        n = self.cube.shape[2]
+        x = self.cube.shape[0]
+        y = self.cube.shape[1]
+        dataz = np.zeros((x,y,n))
+        for i in range(n):
+            if i < window:
+                cube_1 = self.cube[:,:,0:(int(i+window))]
+                window_means = np.mean(cube_1,axis=2)
+                dataz[:,:,i] = window_means
+            elif (n - i) < window:
+                window_means = np.mean(self.cube[:,:,int(i-window):int(n)],axis=2)
+                dataz[:,:,i] = window_means
+            else:
+                window_means = np.mean(self.cube[:,:,int(i-window):int(i+window)],axis=2)
+                dataz[:,:,i] = window_means
+        self.cube = dataz
+        main.create_image(self.cube)
+
 # Resets the normalization parameters when the stretch option changes
 # This needs work - need to add more stretch options and improve parameters for each
     def norm_set(self, text):
@@ -123,15 +235,14 @@ class Main(QMainWindow, Ui_MainWindow):
         if text == 'Log Stretch':
             self.var_max = 10000
             self.var_int = 10
-            self.norm = ImageNormalize(interval=MinMaxInterval(), stretch=self.stretch_val)
+            self.norm = ImageNormalize(interval=self.interval, stretch=self.stretch_val, clip=True)
         elif text == 'No Stretch':
             self.norm = None
         else:
             self.var_max = 10
             self.var_int = 1
-            self.norm = ImageNormalize(interval=MinMaxInterval(),stretch=self.stretch_val)
+            self.norm = ImageNormalize(interval=self.interval, stretch=self.stretch_val, clip=True)
         main.create_image(self.cube)
-
 
 # Displays previous image, prevents errors at boundaries of image set
     def prev(self):
@@ -148,6 +259,12 @@ class Main(QMainWindow, Ui_MainWindow):
         else:
             self.i = self.i + 1
         main.create_image(self.cube)
+
+    def figure_coords(self,event):
+        self.ix = event.xdata
+        self.iy = event.ydata
+        self.lcdX.display(self.ix)
+        self.lcdY.display(self.iy)
 
 # Creates a 3-d ndarray from list of files and creates a
 # list of fits headers
@@ -169,23 +286,31 @@ class Main(QMainWindow, Ui_MainWindow):
 # accompanying date from header
     def create_image(self,cube):
         date = self.dates[self.i]
-        self.image = np.flip(cube[:,:,self.i],axis=0)
-        self.axf1.set_title(date)
-        self.view_image.set_data(self.image)
         self.view_image.set_norm(self.norm)
+        self.image = self.cube[:,:,self.i]
+        imageMin = np.amin(self.image)
+        imageMax = np.amax(self.image)
+        self.view_image.set_data(self.image)
+        self.axf1.set_title(date)
         self.fig1.canvas.draw_idle()
+        self.lcdMin.display(imageMin)
+        self.lcdMax.display(imageMax)
 
 # Connected with Open button, creates interactive dialog box for user to select
 # files to be given to the make_cube function
     def selectFile(self):
         home_dir = str(Path.home())
         files, filter = QtWidgets.QFileDialog.getOpenFileNames(self, "Open Files", " ", "Fits files (*.fits)")
-        files = sorted(files)
-        self.cube = main.make_cube(files)
+        self.files = sorted(files)
+        hdu = fits.open(self.files[0])[0]
+        self.wcs = WCS(hdu.header)
+        self.axf1 = self.canvas.figure.subplots(ncols=1,nrows=1, subplot_kw={'projection': self.wcs})
+        self.axf1.grid()
+        self.cube = main.make_cube(self.files)
         self.cube_base = self.cube
         self.n = self.cube.shape[2]
         self.i = 0
-        self.image = np.flip(self.cube[:,:,0],axis=0)
+        self.image = self.cube[:,:,0]
         self.view_image = self.axf1.imshow(self.image, cmap='gray')
         self.fig1.canvas.draw_idle()
     
